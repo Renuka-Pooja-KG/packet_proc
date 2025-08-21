fba3fd72bc563ae1d63c13d9b60f3b5cb53b3df1
Renuka-Pooja-KG <renuka.pooja@pravegasemi.com>
2025-08-21 15:40:05 +0530

    WRITE_HEADER -> ENQ_REQ=0 READ_HEADER -> DEQ_REQ=0

--- Verification/UVME/sequence/pkt_proc_sequences.sv
+++ Verification/UVME/sequence/pkt_proc_sequences.sv
@@ -443,6 +443,17 @@ class pkt_proc_base_sequence extends uvm_sequence #(pkt_proc_seq_item);
 
   // Read-only scenario with comprehensive write data coverage
   task read_only_scenario();
+    //Read HEADER and go to deq_req = 0 next cycle
+    initialize_dut();
+    write_packet(4, 32'hF600);
+    send_idle_transaction(10);
+    
+    // Read and then go enq_req = 0 next cycle
+    read_data(2);
+
+    send_idle_transaction(5);
+  
+    // Normal write and read scenario
     initialize_dut();
     
     `uvm_info(get_type_name(), $sformatf("Starting read-only scenario with %0d reads and comprehensive write data coverage", num_transactions), UVM_LOW)
@@ -548,6 +559,44 @@ class pkt_proc_base_sequence extends uvm_sequence #(pkt_proc_seq_item);
     
     // Random data with high bit toggle probability
     write_random_data_with_coverage(20);
+
+    send_idle_transaction(10);
+
+    // Write SOP and then go enq_req = 0 next cycle
+    tr = pkt_proc_seq_item::type_id::create("tr_sop");
+    start_item(tr);
+    tr.pck_proc_int_mem_fsm_rstn = 1'b1;
+    tr.pck_proc_int_mem_fsm_sw_rstn = 1'b0;
+    tr.empty_de_assert = 1'b0;
+    tr.enq_req = 1'b1;
+    tr.deq_req = 1'b0;
+    tr.in_sop = 1'b1;
+    tr.in_eop = 1'b0;
+    tr.wr_data_i = 32'hF600;
+    tr.pck_len_valid = 1'b1;
+    tr.pck_len_i = current_packet_length[11:0];
+    tr.pck_proc_almost_full_value = 5'd28;
+    tr.pck_proc_almost_empty_value = 5'd4;
+    finish_item(tr);
+    
+    // Write a few data words
+    
+      tr = pkt_proc_seq_item::type_id::create("tr_data");
+      start_item(tr);
+      tr.pck_proc_int_mem_fsm_rstn = 1'b1;
+      tr.pck_proc_int_mem_fsm_sw_rstn = 1'b0;
+      tr.empty_de_assert = 1'b0;
+      tr.enq_req = 1'b0;
+      tr.deq_req = 1'b0;
+      tr.in_sop = 1'b0;
+      tr.in_eop = 1'b0;
+      tr.wr_data_i = 32'hF600;
+      tr.pck_len_valid = 1'b0;
+      tr.pck_len_i = current_packet_length[11:0];
+      tr.pck_proc_almost_full_value = 5'd28;
+      tr.pck_proc_almost_empty_value = 5'd4;
+      finish_item(tr);
+
     
     `uvm_info(get_type_name(), "Packet write scenario with comprehensive data coverage completed", UVM_LOW)
   endtask
